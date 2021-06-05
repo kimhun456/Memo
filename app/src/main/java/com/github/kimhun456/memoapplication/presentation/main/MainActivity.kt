4a284639c11042480447ce67c15917463c1abe22
Hyunjae Kim <kimhun456@gmail.com>
2021-06-05 20:30:05 +0900

    Add Compose lib and add Memo list for show memo

--- app/src/main/java/com/github/kimhun456/memoapplication/presentation/main/MainActivity.kt
+++ app/src/main/java/com/github/kimhun456/memoapplication/presentation/main/MainActivity.kt
@@ -1,20 +1,101 @@
 package com.github.kimhun456.memoapplication.presentation.main
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.github.kimhun456.memoapplication.R
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.kimhun456.memoapplication.domain.entity.Memo
+import com.github.kimhun456.memoapplication.domain.entity.user.Gender
+import com.github.kimhun456.memoapplication.domain.entity.user.User
+import com.github.kimhun456.memoapplication.presentation.theme.MaterialMemoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+        setContent {
+            MainActivityTheme {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    MemoList(viewModel)
+                    AddButton(viewModel)
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun MainActivityTheme(content: @Composable () -> Unit) {
+        MaterialMemoTheme {
+            Surface(color = Color.LightGray) {
+                content()
+            }
+        }
+    }
+
+    @Composable
+    fun MemoList(viewModel: MainViewModel) {
+        val memoList by viewModel.memoList.observeAsState(listOf())
+        LazyColumn(
+            contentPadding = PaddingValues(top = 8.dp)
+        ) {
+            items(items = memoList) { memo ->
+                MemoCard(memo)
+            }
+        }
+    }
+
+    @Composable
+    fun AddButton(viewModel: MainViewModel) {
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { viewModel.addMemo() }) {
+            Text(text = "Create Memo")
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewMemoCard() {
+        val user = User("", ",", "", Gender.MAN)
+        MemoCard(
+            Memo(
+                title = "title",
+                message = "Hello",
+                author = user,
+                createdTime = 0L,
+                lastModifiedTime = 0L
+            )
+        )
+    }
+
+    @Composable
+    fun MemoCard(memo: Memo) {
+        Column {
+            Text(text = memo.title)
+            Divider(modifier = Modifier.height(1.dp))
+            Text(text = memo.message)
         }
     }
 }
--- app/src/main/java/com/github/kimhun456/memoapplication/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/github/kimhun456/memoapplication/presentation/main/MainViewModel.kt
@@ -1,16 +1,43 @@
 package com.github.kimhun456.memoapplication.presentation.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.kimhun456.memoapplication.domain.entity.Memo
+import com.github.kimhun456.memoapplication.domain.entity.user.Gender
+import com.github.kimhun456.memoapplication.domain.entity.user.User
 import com.github.kimhun456.memoapplication.domain.interactor.CreateUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlin.random.Random
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val createUserUseCase: CreateUserUseCase
 ) : ViewModel() {
 
-    override fun onCleared() {
-        super.onCleared()
+    private val _memoList = MutableLiveData<List<Memo>>(listOf())
+
+    val memoList: LiveData<List<Memo>> = _memoList
+
+    fun addMemo() {
+        val memo = generateRandomMemo()
+        val currentList = _memoList.value!!
+        _memoList.value = currentList.toMutableList().apply { add(memo) }.toList()
+    }
+
+    private fun generateRandomMemo(): Memo {
+        val titles = listOf("To do", "Tonight we party", "My wishList")
+        val messages = listOf("Do coding", "party receipt", "do!!!!!")
+        val index = Random.nextInt(0, titles.size)
+        val user = User("id", "hyunjae", "kimhun456@gmail.com", Gender.MAN)
+        val currentTime = System.currentTimeMillis()
+        return Memo(
+            title = titles[index],
+            message = messages[index],
+            author = user,
+            createdTime = currentTime,
+            lastModifiedTime = currentTime
+        )
     }
 }
